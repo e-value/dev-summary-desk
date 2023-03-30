db511e97c4559074db36a5f57386c6d633aa36cb
e-value <y.miyata506320@gmail.com>
2023-03-30 16:03:00 +0900

    lawモデル作成を「-a」オプションで作成

---
+++ app/Http/Controllers/LawController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLawRequest;
+use App\Http\Requests\UpdateLawRequest;
+use App\Models\Law;
+
+class LawController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLawRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLawRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Law  $law
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Law $law)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Law  $law
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Law $law)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateLawRequest  $request
+     * @param  \App\Models\Law  $law
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateLawRequest $request, Law $law)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Law  $law
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Law $law)
+    {
+        //
+    }
+}
