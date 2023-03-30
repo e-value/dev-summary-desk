5ab856aba5d755aa4972ef1cf1d7db3552cfb737
e-value <y.miyata506320@gmail.com>
2023-03-30 17:59:08 +0900

    composer require psr/simple-cache:^1.0 maatwebsite/excelをインストール

--- app/Http/Controllers/ExportRevisionLawController.php
+++ app/Http/Controllers/ExportRevisionLawController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Imports\RevisionLawsImport;
+use Maatwebsite\Excel\Facades\Excel;
+
 
 class ExportRevisionLawController extends Controller
 {
     public function export()
     {
-        dd("test");
+        return Excel::download(new RevisionLawsImport, 'revisionLaws.xlsx');
     }
 }
--- app/Imports/RevisionLawsImport.php
+++ app/Imports/RevisionLawsImport.php
@@ -5,13 +5,36 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
-class RevisionLawsImport implements ToCollection
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use App\Models\RevisionLaw;
+
+class RevisionLawsImport implements ToCollection,WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
     public function collection(Collection $collection)
     {
-        //
+        dd($collection);
+        return RevisionLaw::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            '詳細',
+        ];
+    }
+
+    public function map($row): array
+    {
+        $data = [
+            $row->id,
+            $row->content,
+        ];
+        dd($data);
+
+        return $data;
     }
 }
