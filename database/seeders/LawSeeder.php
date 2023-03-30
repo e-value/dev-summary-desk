6e943b3199c6994a494754f5476ce4500865e470
e-value <y.miyata506320@gmail.com>
2023-03-31 08:18:11 +0900

    lawSeedrにphpdocsを記載

--- database/seeders/LawSeeder.php
+++ database/seeders/LawSeeder.php
@@ -12,7 +12,9 @@ use App\Models\LawCateogry;
 class LawSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * 5つの方分類にそれぞれに対して法律を5つの法律、計25個の法律学生される
+     * 分類1_a条, 分類1_b条...分類1_e条
+     * 分類2_a条, 分類2_b条...分類2_e条
      *
      * @return void
      */
@@ -26,7 +28,10 @@ class LawSeeder extends Seeder
             'e条',
         ];
 
+        // 5つの法分類を取得
         $categories = LawCategory::all();
+        
+        // 法分類一つ一つに対して、5個の法律を作成
         foreach($categories as $category)
         {
             foreach($names as $name)
