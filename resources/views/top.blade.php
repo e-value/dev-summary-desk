62014dae76ca7d7dd1b74fec99f904a19b6bfc3e
e-value <y.miyata506320@gmail.com>
2023-03-31 10:21:05 +0900

    viteにbootstrap導入

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Summary-Desk</title>
 </head>
 
