804643dc1dc9cbd63c929f717bdcb346dfb733f7
e-value <y.miyata506320@gmail.com>
2023-04-01 11:12:21 +0900

    トップページのcheckboxをbootstrapで整える

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,23 +3,24 @@
 @section('content')
 
 <form action="{{ route('revisionLaws.search') }}" method="get">
-@foreach($law_categories as $law_category)
-<h3>{{ $law_category->name }}</h2>
-    <input type="checkbox" id="law_category_{{$law_category->id}}" value="{{ $law_category->id }}">
-    <div id="laws_belongs_to_category_{{$law_category->id}}" style="display: none;">
-
-        @foreach($law_category->laws as $law)
-
-        <p>{{ $law->name }}</p>
-        <input type="checkbox" name="law_ids[]" value="{{ $law->id }}">
-
+    <div class="form-group">
+        @foreach($law_categories as $law_category)
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox"  id="law_category_{{$law_category->id}}" value="{{ $law_category->id }}">
+                <label class="form-check-label fs-4">{{ $law_category->name }}</label>
+                    <div id="laws_belongs_to_category_{{$law_category->id}}" style="display: none;">
+                        @foreach($law_category->laws as $law)
+                            <input type="checkbox" name="law_ids[]" value="{{ $law->id }}">
+                            <label class="form-check-label">{{ $law->name }}</label>
+                        @endforeach
+                    </div>
+            </div>
         @endforeach
-
     </div>
-    @endforeach
-
     <button id="law_search" class="btn btn-primary">検索</button>
 </form>
+
+
     <table class="table">
         <thead>
             <tr>
