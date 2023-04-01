bedec3876486f2d3f75e39689b5c4058f92495ad
e-value <y.miyata506320@gmail.com>
2023-04-01 10:09:39 +0900

    法分類のチェックボックスを外すと該当の法律のチェックボックスも外れる

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,12 +6,12 @@
 @foreach($law_categories as $law_category)
 <h3>{{ $law_category->name }}</h2>
     <input type="checkbox" id="law_category_{{$law_category->id}}" value="{{ $law_category->id }}">
-    <div class="laws_belongs_to_category_{{$law_category->id}}" style="display: none;">
+    <div id="laws_belongs_to_category_{{$law_category->id}}" style="display: none;">
 
         @foreach($law_category->laws as $law)
 
         <p>{{ $law->name }}</p>
-        <input type="checkbox" name="category_id_{{$law_category->id}}_law_ids[]" value="{{ $law->id }}">
+        <input type="checkbox" name="law_ids[]" value="{{ $law->id }}">
 
         @endforeach
 
