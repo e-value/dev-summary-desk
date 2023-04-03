58d827e1b9ab0c93cc0370b5ca46427f3790b34c
e-value <y.miyata506320@gmail.com>
2023-04-03 12:46:42 +0900

    ajax導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,7 @@
         </main>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
     $(function() {
             $('#select-all-checkbox').click(function() {
